2169a62d32b7e04b5a762b5b08d34b0034ea8c51
yasinkari <yasinibrahim304@gmail.com>
2025-06-03 00:52:25 +0800

    feat: force HTTPS scheme in production environment
    Add URL::forceScheme('https') to ensure all generated URLs use HTTPS in production. This improves security by enforcing encrypted connections.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Providers;
-
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +11,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        //if (app()->environment('production')) {
+        URL::forceScheme('https');
+    }
     }
 
     /**
